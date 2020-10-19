d5496d39115b91690b02fcbdf704489bebfbd4db
RaghulRavichandranR <48898759+RaghulRavichandranR@users.noreply.github.com>
2020-10-19 14:42:16 +1100

    Add files via upload
    First Version - 19 October 2020

---
+++ PIEXCHANGE/build/generated-sources/ap-source-output/RESTWebServices/Customers_.java
@@ -0,0 +1,16 @@
+package RESTWebServices;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-19T14:34:08")
+@StaticMetamodel(Customers.class)
+public class Customers_ { 
+
+    public static volatile SingularAttribute<Customers, String> firstName;
+    public static volatile SingularAttribute<Customers, String> lastName;
+    public static volatile SingularAttribute<Customers, String> phoneNumber;
+    public static volatile SingularAttribute<Customers, Integer> userid;
+
+}
